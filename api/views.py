b8fb418aa49bedd69566cfbb92be1456e7716f05
Ruth <57313608+ruth-cohen89@users.noreply.github.com>
2024-07-02 23:01:29 +0300

    New python api for demo

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def getData(request):
+    person = {'name':'Shani', 'age':28}
+    return Response(person)
+
+
+    
